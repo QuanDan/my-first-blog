642323e612114ce5bfdea136958425934393d045
Quang Dang <quangd4ng@gmail.com>
2016-06-04 07:24:20 -0500

    changed var names

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    #/post/42/
+    url(r'^post/(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,10 @@ from .models import Post
 # Create your views here.
 def post_list(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  #context = {'posts': posts}
   return render(request, 'blog/post_list.html', {'posts': posts})
 
 
-def post_detail(request, pk):
-  post = get_object_or_404(Post, pk=pk)
+def post_detail(request, post_id):
+  post = get_object_or_404(Post, pk=post_id)
   return render(request, 'blog/post_detail.html', {'post': post})
