8a9bfc45350dde4ea0010435b6aeba540d1d1039
Quang Dang <quangd4ng@gmail.com>
2016-06-07 06:03:08 -0500

    Added edit/screate new post buttons

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     #/post/42/
     url(r'^post/(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
-
+    #/post/new/
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<post_id>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
